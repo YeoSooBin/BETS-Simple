d09159d1bcde547458cf5887609b5f936660b49a
YeoSooBin <yeosoobin@gmail.com>
2024-11-06 09:40:33 +0800

    added content in User Journey folder

--- NewsAPI.js
+++ NewsAPI.js
@@ -1,25 +1,28 @@
-const apiKey = 'YOUR_API_KEY';
-const searchQuery = 'technology';
+const button = document.getElementById("myButton");
 
-fetch(`https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    const articles = data.articles;
+const apiKey = '56b226e41e394ce7b5781671326dbdc2';
 
-    // Display the news articles on the page
-    const newsContainer = document.getElementById('news-container');
-    articles.forEach(article => {
-      const articleElement = document.createElement('div');
-      articleElement.innerHTML   
- = `
-        <h2>${article.title}</h2>
-        <p>${article.description}</p>
-        <a href="${article.url}">Read   
- more</a>
-      `;
-      newsContainer.appendChild(articleElement);
+  fetch(`https://newsapi.org/v2/everything?q=${'technology'}&apiKey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const articles = data.articles;
+  
+      // Display the news articles on the page
+      const newsContainer = document.getElementById('news-container');
+      articles.forEach(article => {
+        const articleElement = document.createElement('div');
+        articleElement.innerHTML   
+   = `
+          <h2>${article.title}</h2>
+          <p>${article.description}</p>
+          <a href="${article.url}">Read   
+   more</a>
+        `;
+        newsContainer.appendChild(articleElement);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching news:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching news:', error);
-  });
+
+
